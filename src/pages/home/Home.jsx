712092a3718037c7fda836ac720643aec8db8bbf
Afsana <afsanamirzaei9@gmail.com>
2026-08-08 03:43:50 +0430

    project slider was added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,9 +1,10 @@
 import HeroHome from './HeroHome';
-
+import FeaturedCarousel from'./FeaturedCarousel'
 export default function Home() {
   return (
     <>
       <HeroHome/>
+      <FeaturedCarousel/>
     </>
   );
 }
